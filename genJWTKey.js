610693642831052a495ef24105fe79c60ba56bb5
Kim Jioo <jwkim101201@gmail.com>
2022-01-22 07:41:00 +0900

    jwt is too short

--- genJWTKey.js
+++ genJWTKey.js
@@ -3,7 +3,7 @@ const { appendFile } = require('fs/promises')
 function generateRandomString() {
   let str = ''
 
-  for (let step = 0; step < 50; step++) {
+  for (let step = 0; step < 500; step++) {
     str += String.fromCharCode(Math.floor(Math.random() * 65534 + 1))
   }
 
