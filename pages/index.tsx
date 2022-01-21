9c5ec867e44b570a2c40931f057f38e6b375f36d
Kim Jioo <jwkim101201@gmail.com>
2022-01-21 09:05:29 +0900

    feat: get started btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+
 import styles from '../styles/Home.module.css'
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 const Home: NextPage = () => {
   return (
@@ -23,6 +31,11 @@ const Home: NextPage = () => {
           <br />
           <p className={styles.accent_green}>빠르게</p> 등록하세요.
         </p>
+        <br />
+        <a className={styles.get_started} href="/login">
+          시작하기&nbsp;
+          <FontAwesomeIcon icon={faChevronRight} />
+        </a>
       </div>
     </div>
   )
