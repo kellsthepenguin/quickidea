b55759069d49e3dd8e67ae5839a45e6e48e5451c
Kim Jioo <jwkim101201@gmail.com>
2022-01-23 11:04:41 +0900

    feat(frontend): Login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,45 @@
 import { NextPage } from 'next'
+import toast, { Toaster } from 'react-hot-toast'
 
 import styles from '../styles/Login.module.css'
 
 const Login: NextPage = () => {
+  function login() {
+    console.log('abcd')
+    const mail = (document.getElementById('mail') as HTMLInputElement).value
+    const pw = (document.getElementById('pw') as HTMLInputElement).value
+
+    fetch('/api/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ mail, pw })
+    }).then((res) => res.json()).then(({ token, error }: {
+      token?: string,
+      error?: string
+    }) => {
+      console.log(error)
+      if (error) {
+        toast.error('Login Failed. please check your mail or password.')
+      } else {
+        toast.success('Success!')
+        // TODO: Redirect to Idea List
+      }
+    })
+  }
+
   return (
-    <div className={styles.container}>
-      
+    <div>
+      <Toaster />
+      <div className={styles.container}>
+        <h1>Login</h1>
+        <input placeholder='mail' type='email' id='mail' /> <br />
+        <input placeholder='pw' type='password' id='pw' /> <br />
+        <button onClick={login}>Login</button> <br />
+        or <a href="/register" style={{
+          color: '#1fcf81',
+          fontWeight: 'bold'
+        }}>Register</a>
+      </div>
     </div>
   )
 }
