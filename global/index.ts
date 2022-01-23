68c84f8d1237b2f25e84df1e30f55c3c9e9b829e
Kim Jioo <jwkim101201@gmail.com>
2022-01-23 09:32:33 +0900

    FIXXXXXXX

--- global/index.ts
+++ global/index.ts
@@ -1,7 +1,7 @@
 import prisma from './prisma'
 import { readFileSync } from 'fs'
 
-const key = readFileSync('../private.key')
+const key = readFileSync('./private.key').toString('utf-8')
 
 export {
   prisma,
